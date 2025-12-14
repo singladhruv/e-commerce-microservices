faee53ed5fe7a3535474f6259a12037014cfb440
singladhruv <146742439+singladhruv@users.noreply.github.com>
2025-12-15 04:16:14 +0530

    initial commit

---
+++ product-service/src/main/java/com/example/product_service/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.example.product_service.service;
+
+import com.example.product_service.dto.ProductRequest;
+import com.example.product_service.dto.ProductResponse;
+import com.example.product_service.model.Product;
+import com.example.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor // Auto-generates the constructor for injection
+@Slf4j // Adds a logger automatically
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(this::mapToProductResponse)
+                .toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
